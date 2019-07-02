c4e4bea33128a90b87bb99704c537103208f017a
alexander4387 <52448967+alexander4387@users.noreply.github.com>
2019-07-02 17:06:04 +0300

    Add files via upload

---
+++ scan.js
@@ -0,0 +1,60 @@
+﻿const _serviceUrl = "http://localhost:8000/scanService/";
+
+$(document).ready(function () {
+    var timerId;
+    $("#scan").click(function () {
+        clearInterval(timerId);
+        print(_serviceUrl + "version");
+        $.getJSON(_serviceUrl + "version", function (version) {
+            print("{ version : " + version.version + " }");
+            print(_serviceUrl + "start");
+            $.post({
+                url: _serviceUrl + "start",
+                type: 'POST',
+                contentType: "application/json; charset=utf-8",
+                data: '{"url": "http\://192.168.1.142\:8080/alfresco/UploadHandler.ashx","cookie":"' + document.cookie + '"}',
+                dataType: "json",
+                success: function (data) {
+                    print("{ result : " + data.result + " }");
+                    timerId = setInterval(function () {
+                        print(_serviceUrl + "status");
+                        $.getJSON(_serviceUrl + "status", function (status) {
+                            print("{ isRunning : " + status.isRunning + ", result : " + status.result + " }");
+                            if (status.result) {
+                                print("File uploaded and saved to path " + status.result);
+                                clearInterval(timerId);
+                            } else if (!status.isRunning) {
+                                print("Nasp2 closed");
+                                clearInterval(timerId);
+                            }
+
+                        }).fail(function () {
+                            print("Error on get version");
+                            clearInterval(timerId);
+                        });
+                    }, 1000);
+
+                }
+            }).fail(function () {
+                print("Error on start");
+            });
+        }).fail(function () {
+            print("Error on get version");
+        });
+    });
+
+    $("#close").click(function () {
+        print(_serviceUrl + "close");
+        $.post({
+            url: _serviceUrl + "close",
+            type: 'POST',
+            dataType: "json",
+        });
+    });
+
+    function print(text) {
+        var textarea = $("#notify")[0];
+        textarea.value += text + "\n\r";
+        textarea.scrollTop = textarea.scrollHeight;
+    }
+});
